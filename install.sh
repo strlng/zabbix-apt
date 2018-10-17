fb30963a4b56c4a925ab2ee6e21ccecbc0d369ba
Sterling Anderson <sterling.anderson@wisc.edu>
2018-10-17 11:12:49 -0500

    make sure script runs commands as root

--- install.sh
+++ install.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-sh -c "curl -fsSL https://github.com/strlng/zabbix-apt/archive/master.zip -o zabbix-apt.zip"
+sh_c='sh -c'
+if [ "$user" != 'root' ]; then
+  if command_exists sudo; then
+    sh_c='sudo -E sh -c'
+  else
+    if command_exists su; then
+      sh_c='su -c'
+    else
+      echo "Error: this installer needs the ability to run commands as root."
+      echo "We are unable to find either 'sudo' or 'su' available to make this happen."
+      exit 1
+    fi
+  fi
+fi
 
-sh -c "cp -r zabbix-apt/apt.conf.d/02periodic /etc/apt/apt.conf.d/"
+$sh_c "curl -fsSL https://github.com/strlng/zabbix-apt/archive/master.zip -o zabbix-apt.zip"
 
-sh -c "cp zabbix-apt/zabbix_agentd.d/apt.conf /etc/zabbix/zabbix_agentd.d/"
+$sh_c "unzip zabbix-apt.zip"
 
-sh -c "systemctl restart zabbix-agent.service"
+$sh_c "cp -r zabbix-apt-master/apt.conf.d/02periodic /etc/apt/apt.conf.d/"
 
-sh -c "rm -r zabbix-apt"
+$sh_c "cp zabbix-apt-master/zabbix_agentd.d/apt.conf /etc/zabbix/zabbix_agentd.d/"
+
+$sh_c "systemctl restart zabbix-agent.service"
+
+$sh_c "rm -r zabbix-apt-master zappix-apt.zip"
